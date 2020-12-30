21460db81e9767ee38d5f8c11e035975995d9e5c
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2020-12-30 14:51:31 +0000

    adding functions  to app.js

--- app.js
+++ app.js
@@ -37,3 +37,52 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+const img=document.getElementById('person-img');
+const author=document.getElementById('author');
+const job=document.getElementById('job');
+const info=document.getElementById('info');
+
+const prevBtn=document.querySelector(".prev-btn")
+const nextBtn=document.querySelector(".next-btn")
+const randomBtn=document.querySelector(".random-btn")
+
+//setting first item to display
+let currentItem=0;
+
+//load initial item
+window.addEventListener("DOMContentLoaded", ()=>{
+ showPerson(currentItem);
+})
+
+// show person based on item
+
+const showPerson=(person)=>{
+  const item=reviews[person];
+  img.src= item.img;
+  // img.src= reviews[currentItem].img; same with upper assignment
+  author.textContent=item.name;
+  job.textContent=item.job;
+  info.textContent=item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener("click", ()=>{
+  currentItem++;
+  if(currentItem > reviews.length-1){
+    currentItem=0;
+  }
+  showPerson(currentItem);
+})
+
+// show previous
+
+
+prevBtn.addEventListener("click", ()=>{
+  currentItem--;
+  if(currentItem < 0){
+    currentItem=reviews.length-1;
+  }
+  showPerson(currentItem);
+})
+
